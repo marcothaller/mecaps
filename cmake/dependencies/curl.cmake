55c8a6da2cacc02156416b91d167fc67cb178ae5
Marco Thaller <143189670+marcothaller@users.noreply.github.com>
2024-01-05 18:42:12 +0100

    Update curl.cmake

--- cmake/dependencies/curl.cmake
+++ cmake/dependencies/curl.cmake
@@ -13,22 +13,10 @@ if (NOT TARGET CURL::libcurl)
     set(BUILD_TESTING OFF CACHE BOOL "Turn off testing" FORCE)
     set(BUILD_CURL_EXE OFF CACHE BOOL "Turn off curl executable" FORCE)
 
-    # Force libcurl to be built as shared library despite global BUILD_SHARED_LIBS setting
-    # this is necessary to be able to link faked functions to test executable
-    # option(BUILD_SHARED_LIBS_CURL ON CACHE BOOL "Build libcurl as shared library" FORCE)
-
     if (WIN32)
         set(CURL_USE_SCHANNEL ON CACHE BOOL "Use schannel to build libcurl" FORCE)
     else()
         set(CURL_USE_OPENSSL ON CACHE BOOL "Use OpenSSL to build libcurl" FORCE)
-    endif()
-
-    FetchContent_GetProperties(curl)
-    if(NOT curl_POPULATED)
-        FetchContent_Populate(curl)
-        # add_subdirectory(${curl_SOURCE_DIR} ${curl_BINARY_DIR})
-        set_target_properties(libcurl PROPERTIES BUILD_SHARED_LIBS ON)
-        set_target_properties(libcurl PROPERTIES BUILD_STATIC_LIBS OFF)
     endif()
     
     FetchContent_MakeAvailable(curl)
